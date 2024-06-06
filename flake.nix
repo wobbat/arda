b168a12f9d87dd80074ed68dc91711d0c213ce11
wobbat <wobbat@proton.me>
2024-06-06 14:44:03 +0200

    hm

--- flake.nix
+++ flake.nix
@@ -77,12 +77,9 @@
             home.homeDirectory = "/home/wobbat";
             home.stateVersion = "24.05";
 
-            config = {
-            # Disable if you don't want unfree packages
                 allowUnfree = true;
               # Workaround for https://github.com/nix-community/home-manager/issues/2942
              allowUnfreePredicate = _: true;
-              };
 
           }
         ];
