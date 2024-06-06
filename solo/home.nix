c692af2641439ca1a2b76fb32f75fd3df4e45c66
wobbat <wobbat@proton.me>
2024-06-06 14:41:48 +0200

    hm

--- solo/home.nix
+++ solo/home.nix
@@ -17,12 +17,12 @@
     vscode
   ];
 
-    config = {
+
       # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
-    };
+
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
