e5dfce50c241cdc31bd2c21c30b5c530f7dc8f99
wobbat <wobbat@proton.me>
2024-06-06 14:47:32 +0200

    hm

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
             home.username = "wobbat";
             home.homeDirectory = "/home/wobbat";
             home.stateVersion = "24.05";
-              pkgs.config = {
+              inputs.nixpkgs.config = {
                 allowUnfree = true;
             };
           }
