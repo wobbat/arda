c16a151a6486eaaf76b9abf4eb628b99211bb634
wobbat <wobbat@proton.me>
2024-06-11 10:38:44 +0200

    custom alacritty for solo

--- solo/home.nix
+++ solo/home.nix
@@ -7,7 +7,7 @@
 
 {
   imports = [
-           ../shared/home/alacritty.nix
+           ../solo/alacritty.nix
            ../shared/home/starship.nix
            ../shared/home/fish.nix
            ../shared/home/git.nix
@@ -16,12 +16,12 @@
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    rxvt-unicode
-    vscode
     eza
     nixfmt-rfc-style
-
   ];
+  
+  # trying this to see if the vars are set correctly?
+  programs.bash.enable = true;
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
