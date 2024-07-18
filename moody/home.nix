2bcf073e741e1e8c1a7a1952f5cad3eb6bec1c0d
wobbat <mail@wobbat.com>
2024-07-18 11:37:46 +0200

    moody

---
+++ moody/home.nix
@@ -0,0 +1,81 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    ../shared/home/hyprland.nix
+    ../shared/home/fish.nix
+    ../shared/home/starship.nix
+    ../shared/home/alacritty.nix
+    ../shared/home/git.nix
+    ../shared/home/nixvim.nix
+    ../shared/home/dev.nix
+    ../shared/home/waybar.nix
+  ];
+
+  home.username = "wobbat";
+  home.homeDirectory = "/home/wobbat";
+  
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 8;
+    # "Xft.dpi" = 172;
+  };
+
+   home.pointerCursor = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+      size = 24;
+    };
+
+  #   programs.nixvim = {
+  #   # This just enables NixVim.
+  #   # If all you have is this, then there will be little visible difference
+  #   # when compared to just installing NeoVim.
+  #   enable = true;
+  # };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    eza
+    morgen
+    obsidian
+    microsoft-edge
+    brave
+    #security
+    nmap
+
+    #gui
+    waybar
+    libnotify
+    dunst
+    wbg
+
+    #utils
+    file
+    which
+    btop
+    iotop
+    iftop
+    #other
+    discord
+    signal-desktop
+    nixfmt-rfc-style
+    pavucontrol
+
+    #test
+    inconsolata
+    hermit
+    gnomeExtensions.forge
+  ];
+
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ moody/home/dev.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  home.packages = with pkgs; [
+    go
+    yarn
+    nodejs_22 
+    gnumake
+
+  
+  ];
+}
