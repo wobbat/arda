05cd6db87ff16aa151acace7fc49ecb1ac11d5d7
wobbat <mail@wobbat.com>
2025-06-01 14:27:58 +0200

    fawkes!

--- flake.nix
+++ flake.nix
@@ -10,8 +10,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     helix.url = "github:helix-editor/helix/master";
     wbp.url = "github:wobbat/wobbix_packages";
-    zenbrowser.url = "github:0xc000022070/zen-browser-flake";s
-    zenbrowser.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
