00bf30aca8f10e8ded67944494773ebf87af3e26
wobbat <mail@wobbat.com>
2025-06-01 14:15:06 +0200

    fawkes!

--- hosts/fawkes/main.nix
+++ hosts/fawkes/main.nix
@@ -161,6 +161,7 @@
 
   # Mostly using this for zen-browser
   services.flatpak.enable = true;
+  xdg.portal.enable = true; 
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
   xdg.portal.config.common.default = "gtk";
 
